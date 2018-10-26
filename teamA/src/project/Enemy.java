a7d2ddc539ad88ed978563ed6d83b31179541ce7
kaichihamaishi <s1771083@d-13102-025.ku-kinds.konan-u.ac.jp>
2018-10-26 16:20:16 +0900

    Enemyクラス

---
+++ teamA/src/project/Enemy.java
@@ -0,0 +1,43 @@
+package project;
+
+import framework.game2D.Position2D;
+import framework.game2D.Sprite;
+
+public class Enemy extends Sprite {
+	public Enemy(String imageFile) {
+		super(imageFile);
+	}
+	//‘Ì—Í
+	public int HP=100;
+	//UŒ‚—Í
+	public int power=1;
+	//Œü‚¢‚Ä‚¢‚é•ûŒü,1‚ªã‚ÅŒv‰ñ‚è‚É2,3,4
+	public int way=1;
+
+	public void enemyMove(Position2D player) {
+		float x = 0,y=0;
+		if( this.getPosition().getX() < player.getX() ) {
+			x = 2;
+		}
+		if( this.getPosition().getX() > player.getX() ) {
+			x = -2;
+		}
+
+		if( this.getPosition().getY() < player.getY() ) {
+			y = 1;
+		}
+		if( this.getPosition().getY() > player.getY() ) {
+			y = -1;
+		}
+
+		this.setVelocity(x, y);
+	}
+	public int enemyDamage(int power) {
+		this.HP-=power;
+		return this.HP;
+	}
+	public int getPower() {
+		return this.power;
+	}
+
+}
